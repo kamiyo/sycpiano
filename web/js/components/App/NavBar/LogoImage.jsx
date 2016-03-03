f422e12d08cf423f21b9f328e70901ac1a010ad0
kamiyo <kamiyo@gmail.com>
2016-03-03 02:00:53 -0500

    change to using object for svg (we can manipulate via js and also provide fallback if necessary, not true with img)

--- web/js/components/App/NavBar/LogoImage.jsx
+++ web/js/components/App/NavBar/LogoImage.jsx
@@ -5,7 +5,9 @@ import React from 'react';
 export default class Logo extends React.Component {
     render() {
         return (
-            <img className='logoImage' src='/images/logo.svg' />
+            <object className='logoImage' type='image/svg+xml' data='/images/logo_opt.svg'>
+                {/* fallback goes here */}
+            </object>
         )
     }
 };
