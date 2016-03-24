3c592986e6eeb67330b1d4d1cfc682ad1fc639b8
kamiyo <kamiyo@gmail.com>
2016-03-24 01:06:39 -0400

    try navbar based on scroll, preparing for front page

--- web/js/components/App/App.jsx
+++ web/js/components/App/App.jsx
@@ -4,12 +4,34 @@ import React from 'react';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import NavBar from '@/js/components/App/NavBar/NavBar.jsx';
 import '@/less/animations/route-animation.less';
+import '@/less/animations/navbar-animation.less';
 
 export default class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {isNav: false};
+        this.addNav = this.addNav.bind(this);
+    }
+    addNav() {
+        console.log('addNav');
+        this.setState({isNav: !this.state.isNav});
+    }
+    componentDidMount() {
+        window.addEventListener('wheel', this.addNav);
+    }
     render() {
+        var navbar = this.state.isNav ? <NavBar/> : null;
         return (
             <div className='appContainer'>
-                <NavBar/>
+                <ReactCSSTransitionGroup
+                    transitionName='navbar'
+                    transitionAppear={true}
+                    transitionAppearTimeout={500}
+                    transitionEnterTimeout={500}
+                    transitionLeaveTimeout={500}
+                >
+                    {navbar}
+                </ReactCSSTransitionGroup>
                 <ReactCSSTransitionGroup
                     transitionName='route'
                     transitionAppear={true}
