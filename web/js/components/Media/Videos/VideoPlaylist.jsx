74c58f576c86be65b56658a031ef50b2705237c6
jschen5 <jasonschen5@gmail.com>
2016-04-26 04:00:25 -0400

    use stateless components

--- web/js/components/Media/Videos/VideoPlaylist.jsx
+++ web/js/components/Media/Videos/VideoPlaylist.jsx
@@ -3,26 +3,24 @@ import '@/less/video-playlist.less';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PlaylistManagerHOC from '@/js/components/Media/HigherOrder/PlaylistManagerHOC.jsx';
-import VideoPlaylistItem from '@/js/components/Media/VideoPlaylistItem.jsx';
+import VideoPlaylistItem from '@/js/components/Media/Videos/VideoPlaylistItem.jsx';
 
-class VideoPlaylist extends React.Component {
-    render() {
-        let playlistItems = Object.keys(this.props.items).map((id) => {
-            let item = this.props.items[id];
-            return (
-                <VideoPlaylistItem key={item.id}
-                    isActive={this.props.currentItemId === id}
-                    video={item}
-                    onClick={this.props.playlistItemOnClick} />
-                );
-        });
-
+const VideoPlaylist = (props) => {
+    let playlistItems = Object.keys(props.items).map((id) => {
+        let item = props.items[id];
         return (
-            <div className="videoPlaylist">
-                <ul>{playlistItems}</ul>
-            </div>
+            <VideoPlaylistItem key={item.id}
+                isActive={props.currentItemId === id}
+                video={item}
+                onClick={props.playlistItemOnClick} />
             );
-    }
+    });
+
+    return (
+        <div className="videoPlaylist">
+            <ul>{playlistItems}</ul>
+        </div>
+        );
 }
 
 export default PlaylistManagerHOC(VideoPlaylist);
--- web/js/components/Media/Videos/VideoPlaylistItem.jsx
+++ web/js/components/Media/Videos/VideoPlaylistItem.jsx
@@ -1,37 +1,40 @@
 import React from 'react';
 import moment from 'moment';
 
-export default class VideoPlaylistItem extends React.Component {
-    onClick() {
-        this.props.onClick(this.props.video.snippet.resourceId.videoId);
-    }
+const VideoPlaylistItem = (props) => {
+    let video = props.video;
 
-    render() {
-        let video = this.props.video;
-
-        return (
-            <li className={`videoPlaylistItem${this.props.isActive ? ' active' : ''}`} onClick={this.onClick.bind(this)}>
-                <div className="itemContent">
-                    <div className="imageContainer section">
-                        <img src={video.snippet.thumbnails.high.url}></img>
-                        <span className="duration">
-                            {videoDurationToDisplay(video.contentDetails.duration)}
-                        </span>
-                    </div>
-                    <div className="section videoInfo">
-                        <h4 className="text-top">
-                            {video.snippet.title}
-                        </h4>
-                        <h4 className="text-bottom">
-                            {video.statistics.viewCount} views
-                            | published on {publishedDateToDisplay(video.snippet.publishedAt)}
-                        </h4>
-                    </div>
+    return (
+        <li className={`videoPlaylistItem${props.isActive ? ' active' : ''}`}
+            onClick={() => props.onClick(props.video.snippet.resourceId.videoId)}>
+            <div className="itemContent">
+                <div className="imageContainer section">
+                    <img src={video.snippet.thumbnails.high.url}></img>
+                    <span className="duration">
+                        {videoDurationToDisplay(video.contentDetails.duration)}
+                    </span>
                 </div>
-            </li>
-            );
-    }
+                <div className="section videoInfo">
+                    <h4 className="text-top">
+                        {video.snippet.title}
+                    </h4>
+                    <h4 className="text-bottom">
+                        {video.statistics.viewCount} views
+                        | published on {publishedDateToDisplay(video.snippet.publishedAt)}
+                    </h4>
+                </div>
+            </div>
+        </li>
+        );
 }
+
+VideoPlaylistItem.propTypes = {
+    isActive: React.PropTypes.bool.isRequired,
+    onClick: React.PropTypes.func.isRequired,
+    video: React.PropTypes.object.isRequired
+};
+
+export default VideoPlaylistItem;
 
 
 // Helper functions
