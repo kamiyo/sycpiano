a50bf4174a1b4f88ed34f1c95ea0c7cf4de58807
Andrew Chen <aychen92@gmail.com>
2016-11-18 22:17:34 -0800

    Trying out some calendar packages.

--- web/js/components/Schedule/Schedule.jsx
+++ web/js/components/Schedule/Schedule.jsx
@@ -2,6 +2,7 @@ import '@/less/schedule.less';
 
 import React from 'react';
 
+import Calendar from 'rc-calendar';
 import EventList from '@/js/components/Schedule/EventList.jsx';
 
 export default class Schedule extends React.Component {
@@ -9,7 +10,15 @@ export default class Schedule extends React.Component {
         return (
             <div className="schedule">
                 <div className="schedule__events">
-                    <EventList />
+                    <EventList/>
+                </div>
+                <div className="schedule__calendar">
+                    <Calendar
+                        showDateInput={false}
+                        showToday={false}
+                        enablePrev={true}
+                        enableNext={true}
+                    />
                 </div>
             </div>
         );
