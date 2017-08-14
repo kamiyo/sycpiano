72bd29fc0102d69026f35a0c54e69aa9b1185026
Andrew Chen <aychen92@gmail.com>
2017-08-13 20:40:53 -0700

    Use correct db url in prod in seedDB script.

--- server/models/index.js
+++ server/models/index.js
@@ -1,43 +1,7 @@
 const _ = require('lodash');
 const fs = require('fs');
 const path = require('path');
-const Sequelize = require('sequelize');
-
-const isProduction = process.env.NODE_ENV === 'production';
-
-let secret;
-let host;
-let dbName;
-
-if (!isProduction) {
-    secret = require('../../secret.js');
-    host = 'localhost';
-    dbName = 'sycpiano';
-} else {
-    const dbUrl = process.env.DATABASE_URL;
-    const [
-        entire,
-        username,
-        password,
-        _host,
-        _port,
-        _dbName,
-    ] = dbUrl.match(/postgres:\/\/(.+):(.+)@(.+):(.+)\/(.+)/);
-
-    secret = { username, password }
-    host = _host;
-    dbName = _dbName;
-}
-
-const username = secret.username;
-const password = secret.password;
-
-const sequelize = new Sequelize(dbName, username, password, {
-    host,
-    dialect: 'postgres',
-    pool: { max: 5, min: 0, idle: 10000 },
-    define: { freezeTable: true },
-});
+const sequelize = require('../../sequelize.js');
 
 let db = {};
 
