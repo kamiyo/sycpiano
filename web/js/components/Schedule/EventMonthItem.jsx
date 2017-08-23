bfe8227718ee7352100805dbcf11e8b7ef1f5d7e
kamiyo <kamiyo@gmail.com>
2017-08-22 18:09:08 -0700

    whoops, also change font for calendar.

--- web/js/components/Schedule/EventMonthItem.jsx
+++ web/js/components/Schedule/EventMonthItem.jsx
@@ -3,9 +3,9 @@ import '@/less/Schedule/event-month-item.less';
 import React from 'react';
 
 const EventMonthItem = ({ style, month }) => (
-    <div className="event-month-item" style={this.props.style}>
+    <div className="event-month-item" style={style}>
         <div>
-            {this.props.month}
+            {month}
         </div>
     </div>
 );
