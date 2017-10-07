b1bc9dbdc47752cb26ed6e744a0e08fc40ef0299
kamiyo <kamiyo@gmail.com>
2017-10-07 15:44:16 -0500

    added pause svg

--- web/js/components/Media/Music/IconSVGs.jsx
+++ web/js/components/Media/Music/IconSVGs.jsx
@@ -6,12 +6,16 @@ export const PlaySVG = (props) => (
         height="295.60333"
         width="295.60333"
     >
-        <g
-            transform="translate(-94.493931,-140.23948)"
-        >
-            <path
-                d="m 172.70565,179.34534 -29.32939,16.93333 -29.3294,16.93333 0,-33.86666 0,-33.86667 29.3294,16.93333 z"
-            />
-        </g>
+        <path d="M78.212 39.106L48.882 56.04l-29.33 16.933V5.24l29.33 16.933z" />
+    </svg>
+);
+
+export const PauseSVG = (props) => (
+    <svg {...props} xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 67.733332 67.733335"
+        height="256"
+        width="256"
+    >
+        <path d="M12.43 0h10.583v67.733H12.43zM44.72 0h10.583v67.733H44.72z" />
     </svg>
 );
