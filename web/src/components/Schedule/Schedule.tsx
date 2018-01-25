ba5a3d6d2833d6fa2cb1bc4d6da1385733d7efb8
Andrew Chen <aychen92@gmail.com>
2018-01-25 01:20:50 -0800

    Schedule is completely full of emotion.

--- web/src/components/Schedule/Schedule.tsx
+++ web/src/components/Schedule/Schedule.tsx
@@ -1,23 +1,24 @@
-import 'less/Schedule/schedule.less';
-
 import * as React from 'react';
+import { css } from 'react-emotion';
 import { Redirect, Route, Switch } from 'react-router-dom';
 
-// import ConnectedEventDetails from 'src/components/Schedule/EventDetails';
 import EventList from 'src/components/Schedule/EventList';
+
+import { pushed } from 'src/styles/mixins';
 
 import { DateIconSVG } from 'src/components/Schedule/DateIconSVG';
 import { LocationIconSVG } from 'src/components/Schedule/LocationIconSVG';
 import { TrebleIconSVG } from 'src/components/Schedule/TrebleIconSVG';
 
+const scheduleStyles = css`
+    ${pushed};
+    width: 100%;
+    box-sizing: border-box;
+`;
+
 const Schedule: React.SFC<{}> = () => (
-    <div className="schedule container">
-        {/*
-            <div className="schedule__event-details">
-                <ConnectedEventDetails />
-            </div>
-        */}
-        <div className="schedule__events">
+    <div className={`${scheduleStyles} container`}>
+        <div className={css`height: 100%;`}>
             <Switch>
                 <Route
                     path="/schedule"
@@ -29,20 +30,16 @@ const Schedule: React.SFC<{}> = () => (
                 <Route
                     path="/schedule/upcoming/:date?"
                     exact={true}
-                    render={(routeProps) => {
-                        return (
-                            <EventList {...routeProps} type="upcoming" />
-                        );
-                    }}
+                    render={(routeProps) => (
+                        <EventList {...routeProps} type="upcoming" />
+                    )}
                 />
                 <Route
                     path="/schedule/archive/:date?"
                     exact={true}
-                    render={(routeProps) => {
-                        return (
-                            <EventList {...routeProps} type="archive" />
-                        );
-                    }}
+                    render={(routeProps) => (
+                        <EventList {...routeProps} type="archive" />
+                    )}
                 />
             </Switch>
         </div>
