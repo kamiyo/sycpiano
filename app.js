f93cf37428553be2a7c311435050b1d8054c57c1
Andrew Chen <aychen92@gmail.com>
2018-01-29 22:11:52 -0800

    Add https options and helmet for extra protection.

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const fs = require('fs');
+const helmet = require('helmet');
+const https = require('https');
 const morgan = require('morgan');
 const path = require('path');
 const mustacheExpress = require('mustache-express');
@@ -12,6 +15,16 @@ const port = isProduction ? process.env.PORT : 8000;
 const listenAddr = isProduction ? '0.0.0.0' : '127.0.0.1';
 
 const app = express();
+
+// HTTPS setup
+const httpsOptions = {
+    cert: fs.readFileSync('./sslcert/fullchain.pem'),
+    key: fs.readFileSync('./sslcert/privkey.pem'),
+};
+
+// helmet will add HSTS to force HTTPS connections, remove x-powered-by non-standard header,
+// sets x-frame-options header to disallow our content to be rendered in iframes.
+app.use(helmet());
 
 // .well-known/acme-challenge is the path Certbot uses to verify this webserver.
 // Since we're placing this directory at the project root,
@@ -40,3 +53,6 @@ app.get('/health-check', (req, res) => res.sendStatus(200));
 app.get(/^((?!\/admin).)*$/, (req, res) => res.render('index'));
 
 app.listen(port, listenAddr, () => console.log(`App listening on port ${port}.`));
+
+// Have HTTPS listen on 443
+https.createServer(httpsOptions, app).listen(443);
