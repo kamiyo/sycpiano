500e8d955d21f9a95591634dc50ca764cd2f1f27
kamiyo <kamiyo@gmail.com>
2018-02-06 02:01:03 -0600

    unecessary line break

--- web/src/components/Schedule/EventDetails.tsx
+++ web/src/components/Schedule/EventDetails.tsx
@@ -80,12 +80,10 @@ let EventLocation: React.SFC<EventLocationProps> = (props) => {
         <a href={getGoogleMapsSearchUrl(props.location)} className={props.className} target="_blank">
             <LocationIconInstance className={locationIconStyle} />
 
-            <strong
-                className={css`
-                    color: black;
-                    margin-left: 10px;
-                `}
-            >
+            <strong className={css`
+                color: black;
+                margin-left: 10px;
+            `}>
                 {getVenueName(props.location)}
             </strong>
         </a>
