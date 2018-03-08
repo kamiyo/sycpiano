f5e0b35f0bf66da95160f5145f8e3cfb3db16e35
Sean Chen <kamiyo@gmail.com>
2018-03-08 01:03:38 -0500

    serve resized assets through the resized api

--- server/src/resized.ts
+++ server/src/resized.ts
@@ -1,19 +1,43 @@
 import * as express from 'express';
 import * as path from 'path';
 
-import * as Promise from 'bluebird';
+import * as Sharp from 'sharp';
 
 const resized = express();
 
 resized.get('/*', (req, res) => {
     let imgPath = req.params[0];
     if (!imgPath) {
-        res.end();
+        res.status(404).end();
     }
-    res.write(imgPath);
     imgPath = path.join(__dirname, '../../web/assets/images', imgPath);
 
-    res.end();
+    const w = req.query.w && parseInt(req.query.w, 10);
+    const h = req.query.h && parseInt(req.query.h, 10);
+
+    if (!w && !h) {
+        res.sendFile(imgPath, {}, (err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('sent done');
+            }
+            res.end();
+        });
+    } else {
+        Sharp(imgPath)
+            .resize(w, h)
+            .max()
+            .withoutEnlargement()
+            .toBuffer((err, buff) => {
+                if (err) {
+                    res.status(404).end();
+                }
+                res.contentType('image/jpeg').write(buff, () => {
+                    res.end();
+                });
+            });
+    }
 });
 
 export const Resized = resized;
