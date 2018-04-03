8b905e11aceabdc080756911c37f8e6ccb10862f
kamiyo <kamiyo@gmail.com>
2018-04-02 22:48:43 -0500

    hrm try

--- web/src/components/Media/Music/Music.tsx
+++ web/src/components/Media/Music/Music.tsx
@@ -287,9 +287,8 @@ class Music extends React.Component<MusicProps, MusicState> {
     }
 
     render() {
-        const isMobile = this.props.isMobile;
         return (
-            <div className={musicStyle} onScroll={isMobile ? this.props.onScroll : null}>
+            <div className={musicStyle} onScroll={this.props.isMobile ? this.props.onScroll : null}>
                 <audio id="audio" crossOrigin="anonymous" ref={(audio) => this.audio = audio} />
                 <MusicPlaylist
                     audio={this.audio}
@@ -298,7 +297,7 @@ class Music extends React.Component<MusicProps, MusicState> {
                     baseRoute={this.props.baseRoute}
                     userInput={this.state.userInput}
                     onFirstUserInput={this.onFirstUserInput}
-                    isMobile={isMobile}
+                    isMobile={this.props.isMobile}
                 />
                 <AudioUI
                     seekAudio={this.seekAudio}
@@ -310,14 +309,14 @@ class Music extends React.Component<MusicProps, MusicState> {
                     currentPosition={this.state.playbackPosition}
                     userInput={this.state.userInput}
                     onFirstUserInput={this.onFirstUserInput}
-                    isMobile={isMobile}
+                    isMobile={this.props.isMobile}
                     isLoading={this.state.isLoading}
                 />
                 <AudioInfo
                     duration={this.state.duration}
                     currentPosition={this.state.playbackPosition}
                     currentTrack={this.state.currentTrack}
-                    isMobile={isMobile}
+                    isMobile={this.props.isMobile}
                 />
                 <AudioVisualizer
                     currentPosition={this.state.playbackPosition}
@@ -326,7 +325,7 @@ class Music extends React.Component<MusicProps, MusicState> {
                     duration={this.state.duration}
                     prevTimestamp={this.state.lastUpdateTimestamp}
                     volume={this.state.volume}
-                    isMobile={isMobile}
+                    isMobile={this.props.isMobile}
                 />
             </div>
         );
