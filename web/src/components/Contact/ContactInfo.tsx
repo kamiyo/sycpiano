f1167d41044be29c3cc35db15e32a3e54759af3a
kamiyo <kamiyo@gmail.com>
2019-10-24 19:10:30 -0500

    Revert "Merge branch 'master' into wentworth-contact"
    This reverts commit f668636880e84a4a2933d3e305b8405ee6452b81, reversing
    changes made to 1a15ebb3f77fff5a7bc76e2a6d3c7167f1110928.

--- web/src/components/Contact/ContactInfo.tsx
+++ web/src/components/Contact/ContactInfo.tsx
@@ -135,6 +135,7 @@ const ContactInfo = React.memo((props: ContactInfoProps) => (
         <StyledPersonalContact
             phone={props.phone}
             email={props.email}
+            website={props.website}
         />
     </InfoContainer>
 ));
